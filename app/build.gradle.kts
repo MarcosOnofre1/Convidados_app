31811f1645b0952b32b4bb3b4c2664634e4369fa
Marcos Onofre <marcosxrzz@gmail.com>
2024-06-19 20:51:22 -0300

    :heavy_plus_sign:	build add ROOM and KAPT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.kotlinKapt)
 }
 
 android {
@@ -49,6 +50,12 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
+
+    implementation(libs.kotlinStdlib)
+    implementation(libs.roomRuntime)
+    // lembrando que optamos por usar o kapt, mas o ideal é fazer a mudança para o KSP.
+    kapt(libs.roomCompiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/convidados_app/model/GuestModel.kt
+++ app/src/main/java/com/example/convidados_app/model/GuestModel.kt
@@ -1,5 +1,32 @@
 package com.example.convidados_app.model
 
-data class GuestModel(val id : Int, var name: String, var presence: Boolean ) {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Guest")
+class GuestModel {
+
+    /**
+     ANOTAÇÃO DE CODIGO
+
+     - Mudamos o modelo do database e fizemos aqui
+     antigo :
+     // Cria a tabela chamada Guest
+     db.execSQL("CREATE TABLE " +
+     DataBaseConstants.GUEST.TABLE_NAME + " (" +
+     DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement," +
+     DataBaseConstants.GUEST.COLUMNS.NAME + " text," +
+     DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer);")
+     **/
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id : Int = 0
+
+    @ColumnInfo(name = "name")
+    var name: String = ""
+
+    @ColumnInfo(name = "presence")
+    var presence: Boolean = false
 
 }
